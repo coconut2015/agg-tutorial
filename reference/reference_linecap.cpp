90edd0cc20ac7e197db5ba1518cfb586d179e8dd
Heng Yuan <superduperhengyuan@gmail.com>
2019-10-19 22:28:18 -0700

    Update dash and add reference for linecap

---
+++ reference/reference_linecap.cpp
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2019 Heng Yuan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <agg_conv_stroke.h>
+#include <agg_pixfmt_rgba.h>
+#include <agg_rasterizer_scanline_aa.h>
+#include <agg_renderer_base.h>
+#include <agg_renderer_scanline.h>
+#include <agg_scanline_p.h>
+
+#include "path.h"
+#include "writepng.h"
+
+typedef agg::pixfmt_rgba32  PixelFormat;
+typedef agg::renderer_base<PixelFormat> RendererBaseType;
+
+template<class Path>
+void drawLine (Path& path, agg::line_cap_e lineCap, RendererBaseType& rBase)
+{
+    const agg::rgba8 greenColor (0, 0xff, 0, 0xff);
+
+    agg::rasterizer_scanline_aa<> ras;
+    agg::scanline_p8 scanline;
+
+    agg::conv_stroke<SimplePath> strokePath (path);
+
+    double strokeWidth = 10.0;
+    strokePath.width(strokeWidth);
+    strokePath.line_cap(lineCap);
+    strokePath.line_join(agg::miter_join);
+    strokePath.miter_limit(strokeWidth);
+
+    ras.reset();
+    ras.add_path(strokePath);
+
+    agg::render_scanlines_aa_solid(ras, scanline, rBase, greenColor);
+}
+
+int
+main (int argc, const char* argv[])
+{
+    try
+    {
+        const int imageWidth = 400;
+        const int imageHeight = 100;
+
+        const int pixelSize = PixelFormat::pix_width;
+
+        agg::rendering_buffer   renderBuffer;
+        PixelFormat             pixFmt;
+        RendererBaseType        rBase;
+
+        // Allocate a 100 x 100 rgba buffer
+        unsigned char *imageBuffer = new unsigned char[imageWidth * imageHeight * pixelSize];
+
+        // Hook everything up
+        renderBuffer.attach (imageBuffer, imageWidth, imageHeight, imageWidth * pixelSize);
+        pixFmt.attach(renderBuffer);
+        rBase.attach(pixFmt);
+
+        const agg::rgba8 whiteColor (0xff, 0xff, 0xff, 0xff);
+        const agg::rgba8 blackColor (0x0, 0, 0, 0xff);
+
+        // clear the buffer with transparent white color
+        rBase.clear(whiteColor);
+
+        agg::rasterizer_scanline_aa<> ras;
+        agg::scanline_p8 scanline;
+
+        ras.auto_close(false);
+
+        // draw the bottom half as black
+        {
+            double border[] =
+            {
+                  0,  50,
+                  0, 100,
+                400, 100,
+                400,  50,
+                  0,  50
+            };
+
+            SimplePath path (border, sizeof(border) / sizeof(double));
+
+            ras.reset();
+            ras.add_path(path);
+
+            agg::render_scanlines_aa_solid(ras, scanline, rBase, blackColor);
+        }
+
+        double line1[] = { 100, 100, 100, 50 };
+        double line2[] = { 200, 100, 200, 50 };
+        double line3[] = { 300, 100, 300, 50 };
+
+        SimplePath path1 (line1, sizeof(line1) / sizeof(double));
+        SimplePath path2 (line2, sizeof(line2) / sizeof(double));
+        SimplePath path3 (line3, sizeof(line3) / sizeof(double));
+
+        drawLine (path1, agg::butt_cap, rBase);
+        drawLine (path2, agg::round_cap, rBase);
+        drawLine (path3, agg::square_cap, rBase);
+
+        // now write the image out for visualization
+        char fileName[1000] = { 0 };
+        if (argc > 1)
+        {
+            sprintf (fileName, "%s/", argv[1]);
+        }
+        strcat(fileName, "reference_linecap.png");
+        writePng<RendererBaseType> (fileName, rBase);
+
+        delete imageBuffer;
+    }
+    catch (TutorialException& ex)
+    {
+        printf ("%s\n", ex.getMessage());
+        return 1;
+    }
+    catch (...)
+    {
+        printf ("Unknown exception detected.\n");
+        return 1;
+    }
+
+    return 0;
+}
